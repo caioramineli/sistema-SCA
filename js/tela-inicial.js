66be8a04e34bc0179908162fa8c274b661a661e9
Caio <caiofrancoramineli3@gmail.com>
2023-12-21 00:15:47 -0300

    tela de cadastro de cliente

--- js/tela-inicial.js
+++ js/tela-inicial.js
@@ -1,9 +1,10 @@
 function mudarCor(id){
-    document.getElementsByClassName('select')[id].style.cssText = 'color: rgba(255, 255, 255, 1);'
+    document.getElementsByClassName('select')[id].style.cssText = 'color: rgba(255, 255, 255, 1); transition: 0.3s;'
+    
 }
 
 function desmudarCor(id){
-    document.getElementsByClassName('select')[id].style.cssText = 'color: rgba(255, 255, 255, 0.7);'
+    document.getElementsByClassName('select')[id].style.cssText = 'color: rgba(255, 255, 255, 0.7); transition: 0.3s;'
 }
 
 // document.getElementsByClassName('aba-lateral')[0].style.cssText = 'min-height: 100vh'
